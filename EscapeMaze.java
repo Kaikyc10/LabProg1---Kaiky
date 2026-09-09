8ed97df503d89fa56c423287dba492bd2fd982ca
Kaikyc10 <kaikycostatereza@gmail.com>
2026-09-09 16:56:21 -0300

    Labirinto do barulho

--- EscapeMaze.java
+++ EscapeMaze.java
@@ -16,14 +16,14 @@ public class EscapeMaze {
         } catch (final Exception e){
             //tratamento de erros
         }
-        
+
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("=== Escape Maze ===");
         System.out.println("Você está em uma sala escura...");
-        System.out.println("Informe a direção: ");
-        String direcao = teclado.nextLine();
+        
 
+        /* 
         if (direcao.equalsIgnoreCase("direita")){
             System.out.println("Você avançou pelo corredor!");
         }else if (direcao.equalsIgnoreCase("esquerda")){
@@ -34,6 +34,32 @@ public class EscapeMaze {
         }else{
             System.out.println("Direção inválida!!!");
         }
+
+        */
+        String direcao;
+
+        do{
+            System.out.println("Informe a direção:");
+            direcao = teclado.nextLine();
+        
+            switch (direcao.toLowerCase()) {
+                case "cima":
+                    System.out.println("Você continua na mesma sala");
+                    break;
+                case "baixo":
+                    System.out.println("Você continua na mesma sala");
+                    break;
+                case "direita":
+                    System.out.println("Você avançou pelo corredor!");
+                    break;
+                case "esquerda":
+                    System.out.println("Movimento bloqueado: Há uma parede");
+                    break;
+                default:
+                    System.out.println("Direção inválida");
+                    break;
+        }
+    }while (direcao.equalsIgnoreCase("sair"));
 
         teclado.close();
     }
